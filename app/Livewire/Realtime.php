2115c96af3f331238cbff2ecf90fd9194511a1f3
Nibir Ahmed <nibirahmed9611@gmail.com>
2024-03-15 18:38:36 +0600

    reverb working

--- app/Livewire/Realtime.php
+++ app/Livewire/Realtime.php
@@ -2,20 +2,31 @@
 
 namespace App\Livewire;
 
-use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\Attributes\On;
+use App\Events\RealtimeNotification;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class Realtime extends Component
 {
     use LivewireAlert;
+
+    public $message;
 
     public function render()
     {
         return view('livewire.realtime');
     }
 
-    public function handleRealtimeNotification()
+    public function triggerEvent()
     {
-        $this->alert('success', 'This is a success message');
+        event(new RealtimeNotification($this->message));
+        $this->message = '';
+    }
+
+    #[On('echo:my-channel,RealtimeNotification')]
+    public function handleRealtimeNotification($messages)
+    {
+        $this->alert('success', $messages['message']);
     }
 }
--- resources/views/livewire/realtime.blade.php
+++ resources/views/livewire/realtime.blade.php
@@ -2,8 +2,10 @@
     <div class="container mt-5 pt-5">
         <div class="row">
             <div class="col">
-                <input type="text" class="form-control" placeholder="Your message">
-                <input type="submit" class="btn btn-primary mt-3">
+                <form class="form" wire:submit.prevent='triggerEvent'>
+                    <input wire:model='message' type="text" class="form-control" placeholder="Your message">
+                    <input type="submit" class="btn btn-primary mt-3">
+                </form>
             </div>
         </div>
     </div>
